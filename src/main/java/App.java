936966a9e5ec14bf37a1dddbaed70a2b11ac19c5
ramon <ramon.correa@thomsonreuters.com>
2020-08-16 15:59:12 -0300

    congruente linear

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ public class App {
   public static void main(String[] args) {
     for (int i = 0; i < 10000; i++) {
       System.out.println(
-          RandomNumberGenerator.generateRandomNumber(3L, 10000000000000L));
+          RandomNumberGenerator.generateRandomNumber(3L, 15L, true));
     }
   }
 
--- src/main/java/RandomNumberGenerator.java
+++ src/main/java/RandomNumberGenerator.java
@@ -4,13 +4,13 @@ import java.util.Objects;
 public class RandomNumberGenerator {
 
   static Double seed;
+  final static int CONSTANTE = 4;
 
-  public static Double generateRandomNumber(Long a, Long maxValue) {
+  public static Double generateRandomNumber(Long a, Long maxValue, boolean isCongruenteLinear) {
     if (Objects.isNull(seed)) {
       seed = (double) ZonedDateTime.now().getSecond();
     }
-
-    seed = (a * seed) % maxValue;
+    seed = (a * seed + (isCongruenteLinear ? CONSTANTE : 0)) % maxValue;
     return seed / maxValue;
   }
 
