d4eb088d748202bcb70cada3413367cf6ebe8903
ramon <ramon.correa@thomsonreuters.com>
2020-10-20 13:26:44 -0300

    no-errors

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,19 +4,17 @@ import com.Fila;
 import com.Roteamento;
 import com.YamlConfigPojo;
 import com.evento.Evento;
-
 import com.evento.EventoChegada;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class App {
 
@@ -25,7 +23,7 @@ public class App {
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
     YamlConfigPojo pojo = mapper.readValue(new File("config.yml"), YamlConfigPojo.class);
 
-    Stream<String> linhas = Files.lines(Paths.get("config.txt"));
+//    Stream<String> linhas = Files.lines(Paths.get("config.txt"));
     Double somatorio = 0.0;
 
     for (int i = 0; i < 5; i++) {
@@ -35,8 +33,8 @@ public class App {
       pojo.getQueues().forEach((s, queueConfig) -> {
         filas.add(new Fila(
             s,
-            queueConfig.getCapacity(),
-            queueConfig.getServers(),
+            !Objects.isNull(queueConfig.getCapacity()) ? queueConfig.getCapacity() : null,
+            !Objects.isNull(queueConfig.getServers()) ? queueConfig.getServers(): null,
             queueConfig.getMinArrival(),
             queueConfig.getMaxArrival(),
             queueConfig.getMinService(),
@@ -45,14 +43,18 @@ public class App {
               addAll(pojo.getNetwork()
                   .stream()
                   .filter(outputConfig -> outputConfig.getSource().equals(s))
-                  .map(outputConfig -> new Roteamento(outputConfig.getTarget(), outputConfig.getProbability()))
+                  .map(outputConfig -> new Roteamento(outputConfig.getTarget(),
+                      outputConfig.getProbability()))
                   .collect(Collectors.toList()));
             }}));
       });
 
-      System.out.printf("Execução da %dº simulação: \n\n", i+1);
+      System.out.printf("Execução da %dº simulação: \n\n", i + 1);
 
-      Evento eventoInicial = new EventoChegada(pojo.getArrivals().get(0));
+      Map<String, Integer> arrivals = pojo.getArrivals();
+      String firstQueue = (String) arrivals.keySet().stream().toArray()[0];
+
+      Evento eventoInicial = new EventoChegada(arrivals.get(firstQueue), firstQueue);
       //Evento eventoInicial = new EventoChegada(2.5, 0);
       Escalonador.iniciarEscalonador(eventoInicial);
       Contexto.start(filas);
@@ -63,7 +65,7 @@ public class App {
 
     }
 
-    System.out.println("\nAvarage global time: " + (somatorio/5));
+    System.out.println("\nAvarage global time: " + (somatorio / 5));
   }
 
 }
--- src/main/java/com/evento/EventoChegada.java
+++ src/main/java/com/evento/EventoChegada.java
@@ -4,46 +4,49 @@ import com.Contexto;
 import com.Escalonador;
 import com.Fila;
 import com.GeradorNumeroAleatorio;
-
 import java.util.List;
 
 public class EventoChegada extends EventoAbstract {
 
-    public EventoChegada(double tempo, int filaOrigem) {
-        super(tempo, filaOrigem);
-    }
+  public EventoChegada(double tempo, String filaOrigem) {
+    super(tempo, filaOrigem);
+  }
 
-    @Override
-    public void executa(List<Fila> filas) {
-        contabilizaTempos(filas);
+  @Override
+  public void executa(List<Fila> filas) {
+    contabilizaTempos(filas);
 
-        Fila fila = filas.get(indexFilaOrigem);
+    Fila fila = filas.stream().filter(fila1 -> indexFilaOrigem.equals(fila1.nome)).findFirst()
+        .get();
 
-        if (fila.possuiEspaco()) {
-            fila.adicionarEvento();
-            if (fila.possuiServidorDisponivel()) {
-                String destinoRoteamento = fila.getDestinoRoteamento();
-                if (destinoRoteamento != null) {
-                    Escalonador.agendar(new EventoRoteamento(
-                            Contexto.tempoGlobal + GeradorNumeroAleatorio.getNextEventTime(fila.getTempoSaidaMinimo(), fila.getTempoSaidaMaximo()),
-                            indexFilaOrigem,
-                            destinoRoteamento)
-                    );
-                } else {
-                    Escalonador.agendar(new EventoSaida(
-                            Contexto.tempoGlobal + GeradorNumeroAleatorio.getNextEventTime(fila.getTempoSaidaMinimo(), fila.getTempoSaidaMaximo()),
-                            indexFilaOrigem)
-                    );
-                }
-            }
+    if (fila.possuiEspaco()) {
+      fila.adicionarEvento();
+      if (fila.possuiServidorDisponivel()) {
+        String destinoRoteamento = fila.getDestinoRoteamento();
+        if (destinoRoteamento != null) {
+          Escalonador.agendar(new EventoRoteamento(
+              Contexto.tempoGlobal + GeradorNumeroAleatorio
+                  .getNextEventTime(fila.getTempoSaidaMinimo(), fila.getTempoSaidaMaximo()),
+              indexFilaOrigem,
+              destinoRoteamento)
+          );
         } else {
-            fila.adicionarPerda();
+          Escalonador.agendar(new EventoSaida(
+              Contexto.tempoGlobal + GeradorNumeroAleatorio
+                  .getNextEventTime(fila.getTempoSaidaMinimo(), fila.getTempoSaidaMaximo()),
+              indexFilaOrigem)
+          );
         }
-
-        Escalonador.agendar(new EventoChegada(
-                Contexto.tempoGlobal+ GeradorNumeroAleatorio.getNextEventTime(fila.getTempoChegadaMinimo(), fila.getTempoChegadaMaximo()),
-                indexFilaOrigem)
-        );
+      }
+    } else {
+      fila.adicionarPerda();
     }
+
+    Escalonador.agendar(new EventoChegada(
+        Contexto.tempoGlobal + GeradorNumeroAleatorio
+            .getNextEventTime(fila.getTempoChegadaMinimo(), fila.getTempoChegadaMaximo()),
+        indexFilaOrigem)
+    );
+  }
 
 }
